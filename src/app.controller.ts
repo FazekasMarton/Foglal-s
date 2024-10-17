0e18e2f96649520ceb4142f00b329b21712359e7
FazekasMarton <fazekas.marton16@gmail.com>
2024-10-17 14:35:05 +0200

    jej

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -25,26 +25,11 @@ export class AppController {
   @Get("/order")
   @Render('index')
   validate(@Query() form, @Res() res) {
-    form.error = []
-    let now = new Date()
-    let date = new Date(form.date)
-    console.log(now, date)
-    if(form.name.length <= 0){
-      form.error.push("A név mező nem lehet üres!")
-    }
-    if(!/[a-zA-Z1-9]+@+[a-zA-Z1-9]/.test(form.email)){
-      form.error.push("Nem valid az email cím!")
-    }
-    if(now > date){
-      form.error.push("A foglalás időpontja nem lehet a multban!")
-    }
-    if(!(form.audiance > 0 && form.audiance < 11)){
-      form.error.push("A foglalás 1-10 személyre szólhat!")
-    }
-    if(form.error.length == 0){
+    let result = this.appService.validate(form)
+    if(result.error.length == 0){
       res.redirect("/siker")
     }else{
-      return form;
+      return result;
     }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,24 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  validate(form){
+    form.error = []
+    let now = new Date()
+    let date = new Date(form.date)
+    console.log(now, date)
+    if(form.name.length <= 0){
+      form.error.push("A név mező nem lehet üres!")
+    }
+    if(!/[a-zA-Z1-9]+@+[a-zA-Z1-9]/.test(form.email)){
+      form.error.push("Nem valid az email cím!")
+    }
+    if(now > date){
+      form.error.push("A foglalás időpontja nem lehet a multban!")
+    }
+    if(!(form.audiance > 0 && form.audiance < 11)){
+      form.error.push("A foglalás 1-10 személyre szólhat!")
+    }
+    return form
+  }
 }
